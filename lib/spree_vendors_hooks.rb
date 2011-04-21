00740f553694c90f28b63e4f1015a9525d983e59
Josh Nussbaum <joshnuss@gmail.com>
2011-04-21 16:02:55 -0400

    show source of shipment on admin screens

--- lib/spree_vendors_hooks.rb
+++ lib/spree_vendors_hooks.rb
@@ -4,4 +4,8 @@ class SpreeVendorsHooks < Spree::ThemeSupport::HookListener
   end
   insert_after :admin_products_index_search, :partial => 'admin/products/vendor_search_field'
   insert_after :admin_product_form_right,    :partial => 'admin/products/vendor_field'
+
+  insert_before :admin_shipments_index_headers, :partial => 'admin/shipments/vendor_shipments_index_headers'
+  insert_before :admin_shipments_index_rows,    :partial => 'admin/shipments/vendor_shipments_index_rows'
+  insert_before :admin_shipment_edit_header,    :partial => 'admin/shipments/vendor_shipment_edit_header'
 end
